b6a101552a375140e022e7315ff801dc5ac5588f
lollypop27 <lara@adfactory.co.za>
2016-08-10 20:52:36 +0200

    adding bmi and person files

---
+++ www/js/person.js
@@ -0,0 +1,13 @@
+function Person(attr) {
+  this.weight = attr.weight;
+  this.height = attr.height;
+};
+
+Person.prototype.calculate_bmi = function(method) {
+  calculator = new BMICalculator();
+    if(method == "metric") {
+      calculator.metric_bmi(this);
+    } else {
+      calculator.imperial_bmi(this);
+    }
+};
